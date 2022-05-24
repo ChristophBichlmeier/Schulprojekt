d3e8275785731a4f4fcb8d2127d42f659279a9ef
ChristophBichlmeier <christoph.bichlmeier4@gmail.com>
2022-05-24 17:58:17 +0200

    init

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Christoph!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style= {styles.screens}>
+
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title='Home' color="tomato" style={styles.buttons}/>
+        <Button title='News' color="tomato" style={styles.buttons}/>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +19,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "fff",
+    justifyContent: 'space-evenly',
+    flexDirection: "row",
+    alignItems: 'center'
+  },
+  buttons: {
+    flex: 1,
+  },
+  screens: {
+    flex: 10,
+    backgroundColor: "dodgerblue",
+  }
 });
