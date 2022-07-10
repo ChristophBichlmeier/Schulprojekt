8ba759570ba609395bda1e1dc28c9df6657d1732
ChristophBichlmeier <christoph.bichlmeier4@gmail.com>
2022-07-10 15:42:14 +0200

    Vertretungsplan

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,42 +1,48 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-import HomeScreen from './screens/HomeScreen';
-import InfoScreen from './screens/InfoScreen';
+import HomeScreen from "./screens/HomeScreen";
+import InfoScreen from "./screens/InfoScreen";
+import VertretungsplanScreen from "./screens/VertretungsplanScreen";
 
-const homeName = 'Home';
-const infoName = 'Info';
+const homeName = "Home";
+const infoName = "Info";
+const vertretungsplanName = "Vertretungsplan";
 
 const Tab = createBottomTabNavigator();
 
 export default function MainContainer() {
-    return(
-      <NavigationContainer>
-          <Tab.Navigator 
-          initialRouteName={homeName} 
-          screenOptions={({route}) => ({
-              tabBarIcon: ({focused, color, size}) => {
-                  let iconName;
-                  let rn = route.name;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
 
-                  if (rn === homeName) {
-                      iconName = focused ? 'home' : 'home-outline'
-                  } else if (rn === infoName) {
-                      iconName = focused ? 'list' : 'list-outline'
-                  }
+            if (rn === homeName) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (rn === infoName) {
+              iconName = focused ? "list" : "list-outline";
+            } else if (rn === vertretungsplanName) {
+              iconName = focused ? "list" : "list-outline";
+            }
 
-                  return <Ionicons name={iconName} size={size} color={color}/>
-              }
-          })}>
-
-              <Tab.Screen name={homeName} component={HomeScreen}/>
-              <Tab.Screen name={infoName} component={InfoScreen}/>
-              
-
-          </Tab.Navigator>
-      </NavigationContainer>
-    );
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={infoName} component={InfoScreen} />
+        <Tab.Screen
+          name={vertretungsplanName}
+          component={VertretungsplanScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
