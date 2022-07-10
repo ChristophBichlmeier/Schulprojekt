5652611d8000df5fd64a91ae91976decdcfefb58
ChristophBichlmeier <christoph.bichlmeier4@gmail.com>
2022-07-10 17:27:57 +0200

    r

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,19 +1,18 @@
-import * as React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
 
-export default function HomeScreen({navigation}) {
-    return(
-        <View style = {styles.container}>
-            <Text>This is the Home Screen</Text>
-        </View>
-    )
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>This is the Home Screen</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
