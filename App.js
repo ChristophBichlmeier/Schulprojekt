da38cae77d0d14c1a580be49e255721041b76aec
ChristophBichlmeier <christoph.bichlmeier4@gmail.com>
2022-07-10 14:49:02 +0200

    update

--- App.js
+++ App.js
@@ -1,51 +1,8 @@
-import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import MainContainer from './navigation/MainContainer';
+import * as React from "react";
+import MainContainer from "./navigation/MainContainer";
 
 function App() {
-  return(
-    <MainContainer/>
-  )
+  return <MainContainer />;
 }
 
 export default App;
-
-
-
-
-
-/*export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style= {styles.links}>
-        <Text style={styles.text}>Home</Text>
-      </View>
-      <View style={styles.links}>
-        <Text style={styles.text}>News</Text>
-      </View>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //height: "20%",
-    backgroundColor: "tomato",
-    flexDirection: 'column',
-    justifyContent: "space-evenly",
-    
-  },
-  links: {
-    //flex: 1,
-    height: "20%",
-    backgroundColor: "dodgerblue",
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignContent: "center"
-  },
-  text: {
-    paddingLeft: "10%"
-  }
-});*/
